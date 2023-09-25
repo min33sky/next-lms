a793130277d5279f1da59f29793be44b439560ba
min33sky <min33sky@naver.com>
2023-09-25 14:13:45 +0900

    feat: categories added

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,17 @@
+import prisma from '@/lib/db';
 import React from 'react';
+import Categories from './_components/categories';
 
-export default function SearchPage() {
-  return <div>SearchPage</div>;
+export default async function SearchPage() {
+  const categories = await prisma.category.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 }
