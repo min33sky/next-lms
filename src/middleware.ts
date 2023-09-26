f5e5b0a7ea8e0257567ededb222a6d004661d063
min33sky <min33sky@naver.com>
2023-09-27 00:36:57 +0900

    feat: student dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from '@clerk/nextjs';
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   // debug: true,
-  publicRoutes: ['/', '/api/webhook', '/api/webhook/clerk'],
+  publicRoutes: ['/api/webhook', '/api/webhook/clerk'],
 });
 
 export const config = {
