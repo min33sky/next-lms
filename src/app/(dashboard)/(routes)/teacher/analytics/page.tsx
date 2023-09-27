3ed1ece3add0871d4d07388833e93c9df6928a11
min33sky <min33sky@naver.com>
2023-09-27 21:55:00 +0900

    feat: analytics added

--- src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-export default function AnalyticsPage() {
-  return <div>AnalyticsPage</div>;
+import { DataCard } from './_components/data-card';
+import { Chart } from './_components/chart';
+import getAnalytics from '@/lib/get-analytics';
+
+export default async function AnalyticsPage() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data} />
+    </div>
+  );
 }
---
+++ src/lib/get-analytics.ts
@@ -0,0 +1,59 @@
+import { Course, Purchase } from '@prisma/client';
+import prisma from './db';
+
+type PurchaseWithCourse = Purchase & {
+  course: Course;
+};
+
+const groupByCourse = (purchases: PurchaseWithCourse[]) => {
+  const grouped: { [courseTitle: string]: number } = {};
+
+  purchases.forEach((purchase) => {
+    const courseTitle = purchase.course.title;
+    if (!grouped[courseTitle]) {
+      grouped[courseTitle] = 0;
+    }
+    grouped[courseTitle] += purchase.course.price!;
+  });
+
+  return grouped;
+};
+
+export default async function getAnalytics(userId: string) {
+  try {
+    const purchases = await prisma.purchase.findMany({
+      where: {
+        course: {
+          userId: userId,
+        },
+      },
+      include: {
+        course: true,
+      },
+    });
+
+    const groupedEarnings = groupByCourse(purchases);
+    const data = Object.entries(groupedEarnings).map(
+      ([courseTitle, total]) => ({
+        name: courseTitle,
+        total: total,
+      }),
+    );
+
+    const totalRevenue = data.reduce((acc, curr) => acc + curr.total, 0);
+    const totalSales = purchases.length;
+
+    return {
+      data,
+      totalRevenue,
+      totalSales,
+    };
+  } catch (error) {
+    console.log('[GET_ANALYTICS]', error);
+    return {
+      data: [],
+      totalRevenue: 0,
+      totalSales: 0,
+    };
+  }
+}
